e778766ee6365ce545e952c82f6fbd7798ec22a3
ebukadragon <chukwuebuka.alozie@gompersstudent.org>
2017-06-05 14:57:13 -0700

    Add files via upload

---
+++ RobotHouses.java
@@ -0,0 +1,16 @@
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class RobotHouses {
+	public static void main(String[] args) {
+		Robot dragon= new Robot();
+		dragon.miniaturize();
+		dragon.setSpeed(10);
+		dragon.move(-250);
+		dragon.turn(90);
+		dragon.penDown;
+		
+		
+	}
+}
